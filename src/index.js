536e36039bf369cb90314499127bfc6115535062
Noah Olatoye <noaholatoye@gmail.com>
2019-07-28 01:31:14 +0100

    add project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// React Required
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create Import File
+import "./index.css";
+import HomeHorizontal from "./HomeHorizontal";
+import * as serviceWorker from "./serviceWorker";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Root extends Component {
+	render() {
+		return <HomeHorizontal />;
+	}
+}
+
+ReactDOM.render(<Root />, document.getElementById("root"));
+serviceWorker.register();
